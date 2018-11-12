c539897f82e5438cb3a66e6a38698718a337f20e
camiula <camilacarwalho3@gmail.com>
2018-11-12 13:54:42 -0300

    1 Aula IBGE

--- IBGE/ex1.sql
+++ IBGE/ex1.sql
@@ -20,4 +20,37 @@ FROM municipio m1, municipio m2
 WHERE m2.nm_municip ILIKE 'Cajazeiras'
 AND ST_Distance(ST_Centroid(m1.geom),ST_Centroid(m2.geom))*40075/360 <30
 AND m1.nm_municip not ILIKE 'Cajazeiras'
+--Distância entre Cajazeiras e João Pessoa
+SELECT ST_Distance(ST_Centroid(m1.geom), ST_Centroid(m2.geom)) * 40075/360
+FROM municipio m1, municipio m2
+WHERE m1.nm_municip ilike 'cajazeiras'
+AND m2.nm_municip ilike 'joão pessoa'
+
+--Maior cidade da mesorregião sertão paraibano
+SELECT m.nm_municip, ST_Area(m.geom) * ((40075/360)^2) as area
+FROM municipio m, mesorregiao me
+WHERE me.nm_meso ilike 'sertão paraibano'
+AND ST_Contains(me.geom,ST_Centroid(m.geom))
+ORDER BY area DESC
+LIMIT 1
+
+-Cidade da Paraíba com maior área
+SELECT m.nm_municip, ST_Area(m.geom) * ((40075/360)^2) as area
+FROM municipio m, estado e
+WHERE e.nm_estado ilike 'paraíba'
+AND ST_Contains(e.geom,ST_Centroid(m.geom))
+ORDER BY area DESC
+LIMIT 1
+
+--Cidade da Paraíba com maior perímetro
+SELECT m.nm_municip, ST_Area(m.geom) * (40075/360) as perimetro
+FROM municipio m, mesorregiao me
+WHERE me.nm_meso ilike 'sertão paraibano'
+AND ST_Contains(me.geom,ST_Centroid(m.geom))
+ORDER BY perimetro DESC
+LIMIT 1
+--Dado um ponto, retornar o nome do município que ele está contido
+SELECT nm_municip
+FROM municipio
+WHERE ST_Contains(geom, ST_GeomFromText('POINT(-38.544732 -6.889714 )'))
 
